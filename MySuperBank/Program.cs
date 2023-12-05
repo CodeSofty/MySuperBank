792361a842ded5787164d324eb7863a437fd4b53
CodeSoftly <eclittlejohn@yahoo.com>
2023-12-04 23:37:19 -0600

    learning about creating objects in C#

--- MySuperBank/Program.cs
+++ MySuperBank/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MySuperBank;
+
+var account = new BankAccount("Kendra", 1000);
+
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
