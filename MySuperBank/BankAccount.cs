8bbc4aa3672691147f6b90d3ff0da7b7f6167c70
CodeSoftly <eclittlejohn@yahoo.com>
2023-12-05 22:31:23 -0600

    Finished C# OOP Tutorial

--- MySuperBank/BankAccount.cs
+++ MySuperBank/BankAccount.cs
@@ -1,22 +1,72 @@
+using System.Text;
+
 namespace MySuperBank;
 
 public class BankAccount
 {
     public string Number { get; }
     public string Owner { get; set; }
-    public decimal Balance { get; }
+
+    public decimal Balance
+    {
+        get
+        {
+            decimal balance = 0;
+            foreach (var item in _allTransactions)
+            {
+                balance += item.Amount;
+            }
+
+            return balance;
+        }
+    }
+
+    private List<Transaction> _allTransactions = new List<Transaction>();
+
+    private static int s_accountNumberSeed = 123456789;
 
     public BankAccount(string name, decimal initialBalance)
     {
         Owner = name;
-        Balance = initialBalance;
+        MakeDeposit(initialBalance, DateTime.Now, "Initial Deposit");
+        Number = s_accountNumberSeed.ToString();
+        s_accountNumberSeed++;
     }
     
     public void MakeDeposit(decimal amount, DateTime date, string note)
     {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+        }
+        var deposit = new Transaction(amount, date, note);
+        _allTransactions.Add(deposit);
     }
 
     public void MakeWithdrawal(decimal amount, DateTime date, string note)
     {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+        }
+        if (Balance - amount < 0)
+        {
+            throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+        }
+        var withdrawal = new Transaction(-amount, date, note);
+        _allTransactions.Add(withdrawal);
+    }
+    public string GetAccountHistory()
+    {
+        var report = new StringBuilder();
+        //HEADER
+        report.AppendLine("Date\t\tAmount\tNote");
+        foreach (var item in _allTransactions)
+        {
+         //ROWS
+         report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{item.Notes}");
+        }
+
+        return report.ToString();
     }
 }
--- MySuperBank/Program.cs
+++ MySuperBank/Program.cs
@@ -3,3 +3,33 @@
 var account = new BankAccount("Kendra", 1000);
 
 Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+account.MakeWithdrawal(400, DateTime.Now, "Car Repairs");
+
+// Test that the initial balances must be positive.
+BankAccount invalidAccount;
+
+account.MakeWithdrawal(50, DateTime.Now, "Xbox Game");
+Console.WriteLine(account.GetAccountHistory());
+
+// Test for a negative balance.
+// try
+// {
+//     account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+// }
+// catch (InvalidOperationException e)
+// {
+//     Console.WriteLine("Exception caught trying to overdraw");
+//     Console.WriteLine(e.ToString());
+// }
+//
+// // Test that the initial balances must be positive.
+// try
+// {
+//     invalidAccount = new BankAccount("invalid", -55);
+// }
+// catch (ArgumentOutOfRangeException e)
+// {
+//     Console.WriteLine("Exception caught creating account with negative balance");
+//     Console.WriteLine(e.ToString());
+//     return;
+// }
